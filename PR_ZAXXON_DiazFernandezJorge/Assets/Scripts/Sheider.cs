5dfc31343a0b56f68f58415f8e088a627911d1d6
Vilkiin <jorgeeldiaz08@gmail.com>
2022-02-04 20:49:55 +0100

    materiales y skydome y mas

---
+++ PR_ZAXXON_DiazFernandezJorge/Assets/Scripts/Sheider.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sheider : MonoBehaviour
+{
+
+    [SerializeField] Material mat;
+    [SerializeField]  float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = -8f;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mat.SetFloat("Vector1_36714c9cee7b422984024c19eb3681ce", speed);
+    }
+}
