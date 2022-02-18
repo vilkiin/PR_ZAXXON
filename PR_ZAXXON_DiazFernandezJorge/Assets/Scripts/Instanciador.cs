44085cc738752f921cb3721fc2cca672a0ebd998
Vilkiin <jorgeeldiaz08@gmail.com>
2022-02-18 21:00:56 +0100

    mejorado instanciar

--- PR_ZAXXON_DiazFernandezJorge/Assets/Scripts/Instanciador.cs
+++ PR_ZAXXON_DiazFernandezJorge/Assets/Scripts/Instanciador.cs
@@ -9,13 +9,18 @@ public class Instanciador : MonoBehaviour
     float intervalo;
     [SerializeField] GameObject[] obstaculos;
     [SerializeField] Transform instantiatePos;
+    [SerializeField] Transform instantiatePos2;
+    [SerializeField] Transform instantiatePos3;
 
+    float limiteR = 24f;
+    float limiteL = -24f;
     // Start is called before the first frame update
     void Start()
     {
         intervalo = 0.5f;
         StartCoroutine("CrearColumna");
-        
+        //StartCoroutine("CrearColumna2");
+        //StartCoroutine("CrearColumna3");
     }
 
     // Update is called once per frame
@@ -31,7 +36,66 @@ public class Instanciador : MonoBehaviour
         {
             int numAl = Random.Range(0, obstaculos.Length);
             float randomX = Random.Range(-24f, 24f);
+            if (obstaculos[numAl].tag != "Pared")
+            {
+                randomX = Random.Range(-24f, 24f);
+            }
+            else
+            {
+                randomX = 0f;
+            }
             Vector3 newPos = new Vector3(randomX, instantiatePos.position.y, instantiatePos.position.z);
+            Instantiate(obstaculos[numAl], newPos, Quaternion.identity);
+
+            yield return new WaitForSeconds(intervalo);
+
+
+        }
+
+
+    }
+    IEnumerator CrearColumna2()
+    {
+
+        while (true)
+        {
+            int numAl = Random.Range(0, obstaculos.Length);
+            float randomX = Random.Range(-24f, 24f);
+            if (obstaculos[numAl].tag != "pared")
+            {
+                randomX = Random.Range(-24f, 24f);
+            }
+            else
+            {
+                randomX = 0f;
+            }
+            Vector3 newPos = new Vector3(randomX, instantiatePos2.position.y, instantiatePos2.position.z);
+            Instantiate(obstaculos[numAl], newPos, Quaternion.identity);
+
+            yield return new WaitForSeconds(intervalo);
+
+
+        }
+
+
+    }
+
+    IEnumerator CrearColumna3()
+    {
+
+        while (true)
+        {
+            int numAl = Random.Range(0, obstaculos.Length);
+            float randomX = Random.Range(-24f, 24f);
+            if (obstaculos[numAl].tag != "pared")
+            {
+                randomX = Random.Range(-24f, 24f);
+            }
+            else
+            {
+                randomX = 0f;
+            }
+            Vector3 newPos = new Vector3(randomX, instantiatePos3.position.y, instantiatePos3.position.z);
             Instantiate(obstaculos[numAl], newPos, Quaternion.identity);
 
             yield return new WaitForSeconds(intervalo);
@@ -44,9 +108,6 @@ public class Instanciador : MonoBehaviour
 
 
 
-
-
-    
 
 
 
